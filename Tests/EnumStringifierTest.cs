be439f9b5f641c1dba1765a88c11ff66fad555f7
Matyáš Holub <17685682+NocturneCZ@users.noreply.github.com>
2022-01-22 20:09:31 +0100

    Added EnumStringifier tests

---
+++ Tests/EnumStringifierTest.cs
@@ -0,0 +1,47 @@
+﻿using NokLib;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class EnumStringifierTest
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            Assert.AreEqual(ConsoleColor.White.ToString(), EnumStringifier.ToString(ConsoleColor.White));
+        }
+
+        [TestMethod]
+        public void Complex()
+        {
+            var values = Enum.GetValues(typeof(ConsoleColor)) as ConsoleColor[];
+
+            for (int i = 0; i < values.Length; i++) {
+                Assert.AreEqual(values[i].ToString(), EnumStringifier.ToString(values[i]));
+            }
+        }
+
+        [TestMethod]
+        public void MultipleSimple()
+        {
+            Assert.AreEqual(ConsoleColor.White.ToString(), EnumStringifier.ToString(ConsoleColor.White));
+            Assert.AreEqual(DayOfWeek.Monday.ToString(), EnumStringifier.ToString(DayOfWeek.Monday));
+        }
+
+        [TestMethod]
+        public void MultipleComplex()
+        {
+            var colors = Enum.GetValues(typeof(ConsoleColor)) as ConsoleColor[];
+            var days = Enum.GetValues(typeof(DayOfWeek)) as DayOfWeek[];
+            foreach (var item in colors.Zip(days)) {
+                (var color, var day) = item;
+                Assert.AreEqual(color.ToString(), EnumStringifier.ToString(color));
+                Assert.AreEqual(day.ToString(), EnumStringifier.ToString(day));
+            }
+        }
+    }
+}
