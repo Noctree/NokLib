32819c04d0b9545be9406d79de7172be95f41b73
Matyáš Holub <17685682+NocturneCZ@users.noreply.github.com>
2022-06-06 21:17:56 +0200

    - Added Benchmarks to source control

---
+++ Benchmarks/IntParserBenchmark.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using NokLib.Parsers;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class IntParserBenchmark
+    {
+        public static readonly string IntMaxValue = int.MaxValue.ToString();
+        public static readonly string IntMinValue = int.MinValue.ToString();
+        [Benchmark]
+        public void IntParseStandard() => int.TryParse(IntMaxValue, out int result);
+        [Benchmark]
+        public void IntParse() => FastIntParser.TryParseS(IntMaxValue, out int result);
+        [Benchmark]
+        public void IntParseStandardMin() => int.TryParse(IntMinValue, out int result);
+        [Benchmark]
+        public void IntParseMin() => FastIntParser.TryParseS(IntMinValue, out int result);
+    }
+}
---
+++ Benchmarks/UnsafeConversionBenchmark.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using NokLib;
+
+namespace Benchmarks
+{
+    public class UnsafeConversionBenchmark
+    {
+        [Params(true, false)]
+        public bool Boolean { get; set; }
+
+        [Params(ConsoleColor.White)]
+        public ConsoleColor Enum { get; set; }
+
+        [Benchmark]
+        public int BooleanAsInt() => Boolean.AsInt();
+
+        [Benchmark]
+        public int BooleanAsIntFast() => Boolean.AsIntFast();
+
+        [Benchmark]
+        public int EnumAsInt() => Enum.AsInt();
+        [Benchmark]
+        public int EnumAsIntFast() => Enum.AsIntFast();
+    }
+}
