fbb782e7f349bade3a0ad2a64e22fed1a9159550
Matyáš Holub <17685682+NocturneCZ@users.noreply.github.com>
2022-11-10 18:34:28 +0100

    - Moved latest_stable_autorelease into workflows folder - UnsafeExtensions.ToIntFast now has an optional Force argument, this argument allows for precision loss while casting

--- Source/Unsafe/UnsafeExtensions.cs
+++ Source/Unsafe/UnsafeExtensions.cs
@@ -40,17 +40,22 @@ namespace NokLib
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="value"></param>
+        /// <param name="force">Force conversion of types too large to fit into an int, CAN RESULT IN DATA LOSS</param>
         /// <exception cref="TypeMismatchException">Thrown when the enum type is backed by an 8 byte value (eg. long/ulong), as int fits only 4 bytes</exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static unsafe int ToIntFast<T>(this T value) where T : unmanaged, Enum {
+        public static unsafe int ToIntFast<T>(this T value, bool force = false) where T : unmanaged, Enum {
             if (sizeof(T) == 1)
                 return *(byte*)(&value);
             else if (sizeof(T) == 2)
                 return *(short*)(&value);
             else if (sizeof(T) == 4)
                 return *(int*)(&value);
-            else if (sizeof(T) == 8)
-                throw new UnsupportedTypeException("Enums of type long/ulong are not supported (too large to fit in int)");
+            else if (sizeof(T) == 8) {
+                if (force)
+                    return (int)(*(int*)(&value));
+                else
+                    throw new UnsupportedTypeException("Enums of type long/ulong are not supported (too large to fit in int)");
+            }
             else
                 throw new ArgumentException("Argument is not a usual enum type; it is not 1, 2, 4, or 8 bytes in length.");
         }
